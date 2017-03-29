3ec9b6dcabe0e6f4a4bbe88468bdd4a0cbe7f48f
govindarajanv <govindarajanv@gmail.com>
2017-03-29 23:11:04 +0530

    Additional changes

--- RubyIn100Minutes/HelloRuby.rb
+++ RubyIn100Minutes/HelloRuby.rb
@@ -61,7 +61,109 @@ class HelloRuby
     # Another version
     
     5.times{ puts "Hello, World - Version#2!" } 
+      
+    puts " "
+
     
+    #There are many methods that accept blocks. Like the .gsub method you saw on String earlier will run a block once for each match:
+    print "this is a sentence".gsub("e"){ puts "Found an E!"}  
+    puts " "
+    #Block Parameters
+    5.times do |i|
+      puts "#{i}: Hello, World!"
+    end  
+    
+    puts " "
+    print "this is a sentence".gsub("e"){|letter| letter.upcase}
+      
+    #Arrays
+    meals = ["Breakfast", "Lunch", "Dinner"]
+    puts " "
+    print meals
+    puts " "
+    # Appending to an array using Shovel operator
+    meals << "Dessert"
+    puts meals
+    puts " "
+    print meals[2]
+    puts " "
+    print meals.last
+    puts " "
+    print meals.first()
+    puts " "
+    print meals[1..3]
+    puts " "
+    # raise an error use fetch, out of array bounds
+    print meals.fetch(100,"This is message when error is thrown")
+    # first n elements, use take
+    puts " "
+    print meals.take(2)
+    # Drop is opposite of take
+    puts " "
+    print meals.drop(2)
+    # Exists
+    puts " "
+    print " Desserts exists?"
+    print meals.include?('Dessert')
+    
+    #Operations on Arrays
+    puts " "
+    puts " --- Sorted Array --- "
+    print meals.sort
+    # Iterating through Array
+    
+    puts " "
+    meals.each { |a| print a + " " }
+    words = %w[first second third fourth fifth sixth]  
+    puts " "
+    
+    words.reverse_each { |word| print " word iterated is #{word} " }
+    puts " "  
+    puts "Index usage "
+    print words.index("fifth") 
+    
+    # Hashes
+    produce = {"apples" => 3, "oranges" => 1, "carrots" => 12}
+    puts " "  
+      
+    puts "There are #{produce['oranges']} oranges in the fridge."  
+    
+    puts " "
+    print produce.keys()
+    puts " "
+    print produce.values()
+    
+    # Hashes another version
+    
+    produce_1 = {apples: 3, oranges: 1, carrots: 12}
+    puts " "  
+      
+    puts "There are #{produce_1[:oranges]} oranges in the fridge."  
+    
+    puts " "
+    print produce_1.keys()
+    puts " "
+    print produce_1.values()
+     
+    
+    
+    # nil is Ruby’s way of referring to "nothingness."
+    
+    puts " "
+    symbol_1 = {
+            :site => "opscode.com",
+             :ports => [ 80, 443 ]
+             }
+    print symbol_1         
+    
+    # Methods
+    puts " "
+    print "I like Chef".gsub(/like/, "love") 
+    puts " "
+    print FileTest.exists?("/etc/passwd")
+    
+          
+
     
     
     
@@ -79,9 +181,34 @@ class HelloRuby
     
     
   end
-  
+  # conditional
+    
+    def water_status(minutes)
+      if minutes < 7
+        puts "The water is not boiling yet."
+      elsif minutes == 7
+        puts "It's just barely boiling"
+      elsif minutes == 8
+        puts "It's boiling!"
+      else
+        puts "Hot! Hot! Hot!"
+      end
+    end
+    
+    def package_to_picked(platform)
+      
+      puts " "
+      case platform
+      when "debian", "ubuntu"
+        print "apache2"
+      when "centos", "redhat"
+        print "httpd"
+      end
+  end
 end
 
 #Dont use camel case instead use snake case with _ in between words
 h = HelloRuby.new()
 h.hello()
+h.water_status(4)
+h.package_to_picked("debian")
