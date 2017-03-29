a5a134971f2e48e03d3e48470b135e4f0a60f8ba
govindarajanv <govindarajanv@gmail.com>
2017-03-29 17:47:17 +0530

    Ruby Hands on programs

---
+++ HelloWorld.rb
@@ -0,0 +1,20 @@
+puts "40".reverse
+
+code_sub = "code substitution"
+puts "This is a string with #{code_sub}"
+
+print [ "apache", "php", "mysql" ]
+
+print [ "apache", "php", "mysql" ].sort
+
+print %w{apache php mysql}.sort
+
+
+
+ a = {
+		"name" => "govind",
+		"team" => "Marketplaces",
+		"vehicles" => [ "bike","car" ]
+		}
+
+print a
