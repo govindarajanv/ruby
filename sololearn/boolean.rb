b1b35a2221e709a1516ad3abae24819ce3454db2
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-05-15 09:56:46 +0530

    ruby practice exercises

---
+++ sololearn/boolean.rb
@@ -0,0 +1,4 @@
+#Only true and false are Booleans. nil is not a Boolean. 0 is not a Boolean. The string "Hello" is not a Boolean.
+#
+
+puts "False"
---
+++ sololearn/control_structures.rb
@@ -0,0 +1,21 @@
+puts "Enter your age"
+age=gets.chomp.to_i
+if age >= 18 
+  puts "You are eligible to case vote"
+elsif age >=17 && age < 18
+  puts "Please wait for one year, you would be eligible soon"
+else 
+  puts "Too young to cast your vote"
+end
+
+#unless can be used for the same
+unless age >= 18
+  puts "Too young to cast your vote"
+else
+  puts "You are eligible to case vote"
+end
+
+a = 25
+puts "Yes, greater than 10" if a > 10
+
+puts "Yes, greater than 10" unless a < 10
