1f280fdc8b5f8c88679db018f8cd1fe57eb113ee
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-05-25 11:56:22 +0530

    ruby practice exercises

--- sololearn/loops.rb
+++ sololearn/loops.rb
@@ -13,3 +13,37 @@ until a > 10
   puts "a = #{a}"
   a +=2
 end
+
+
+#for loops
+#
+for i in (1..10)
+  break if i == 3
+  puts i
+end
+#
+#for loops
+#
+for i in (1..10)
+  next if i == 3
+  puts i
+end
+
+#for loops
+cnt = 0
+for i in (1..10)
+  if i == 3 
+     cnt += 1
+  else
+     cnt = 0
+  end
+  puts i
+  redo if i == 3 && cnt <= 1
+end
+
+x = 0
+loop do
+  puts x
+  x += 1
+  break if x > 10
+end
