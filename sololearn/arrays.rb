28f8a3eb712be0acb0b9065e6a94eff705d05ec0
Govindarajan Vishnuchithan <govindarajanv@gmail.com>
2018-05-27 21:44:40 +0530

    sololearn ruby exercises

--- sololearn/arrays.rb
+++ sololearn/arrays.rb
@@ -40,3 +40,52 @@ print items
 
 print items[2..4]
 
+# Array manipulation
+#
+a = [1,3,5]
+b = [2,4,6]
+puts ""
+print a+b
+puts ""
+print a*2
+puts ""
+c = a + [5]
+print c
+puts ""
+print c+a
+puts ""
+print a&c
+puts ""
+print a|b
+puts ""
+print (a|b).reverse
+puts ""
+print c.reverse
+puts ""
+print c
+puts ""
+# make reversal permanent
+print c.reverse!
+puts ""
+print c
+
+# Other methods
+
+puts ""
+print c.length
+puts ""
+print c.sort
+puts ""
+print c.uniq
+puts ""
+print c.uniq!
+puts ""
+print c.freeze
+puts ""
+print c.min
+puts ""
+print c.max
+puts ""
+print c.include?(4)
+puts ""
+print c.size
