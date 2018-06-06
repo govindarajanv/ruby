fba4fe14b7f45773bd04995a6be3dc6805214dd4
Govindarajan Vishnuchithan <Govindarajan.Vishnuchithan@altisource.com>
2018-06-06 21:15:28 +0530

    sololearn ruby exercises

--- sololearn/proc.rb
+++ sololearn/proc.rb
@@ -7,3 +7,19 @@ end
 
 greet.call "World"
 greet.call "Govind"
+
+greet = Proc.new do |x|
+  puts "Welcome #{x}"
+end
+
+goodbye = Proc.new do |x|
+  puts "Goodbye #{x}"
+end
+
+def say(arr, proc)
+  arr.each { |x| proc.call x}
+end
+
+people = ["David", "Amy", "John"]
+say(people, greet)
+say(people, goodbye)
