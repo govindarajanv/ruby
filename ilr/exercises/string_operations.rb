f1bdde0f3464528a4aa5957efc652d534e94d1b2
govindarajanv <govindarajanv@gmail.com>
2018-06-17 19:02:45 +0530

    ILR ruby exercises

---
+++ ilr/exercises/string_operations.rb
@@ -0,0 +1,9 @@
+str = "I am Govind "
+puts "reference string is " + str
+puts "reversing the string " + str.reverse
+puts "length is " + str.length.to_s
+puts "upper case of the string is " + str.upcase
+puts "lower case of the string is " + str.downcase
+puts "is the string empty? " + str.empty?.to_s
+puts "print the string three times " + str*3
+puts "concatenating a new line character as such " + str + '\\n'
