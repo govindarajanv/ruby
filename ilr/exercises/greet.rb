95be369e98c931b067b18a207aa6b82cf225fef6
govindarajanv <govindarajanv@gmail.com>
2018-06-19 10:07:02 +0530

    ilr ruby exercises

---
+++ ilr/exercises/greet.rb
@@ -0,0 +1,5 @@
+puts "Hello!"
+print "Enter your name:"
+name = gets()
+print "Hello #{name}."
+print " I am Ruby. Hope you will get required expertise by learning Ruby well."
--- ilr/exercises/string_operations.rb
+++ ilr/exercises/string_operations.rb
@@ -7,3 +7,9 @@ puts "lower case of the string is " + str.downcase
 puts "is the string empty? " + str.empty?.to_s
 puts "print the string three times " + str*3
 puts "concatenating a new line character as such " + str + '\\n'
+=begin
+This is the multiline statement in 
+Ruby language
+This will be helpful for
+developers
+=end
