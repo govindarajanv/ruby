4f354ba00ec4d30af963c8f68d87f9e7c96c2950
govindarajanv <govindarajanv@gmail.com>
2018-06-26 23:02:13 +0530

    ilr ruby exercises

--- ilr/exercises/loops.rb
+++ ilr/exercises/loops.rb
@@ -28,3 +28,35 @@ puts " "
 10.step 1, -1 do |x|
  print "#{x},"
 end
+
+#while loop
+i = 2
+puts ""
+
+while i<= 100 do
+  p i
+  i+= 2
+end
+
+#until loop
+i = 2
+puts ""
+
+until i> 100 do
+  p i
+  i+= 2
+end
+
+#next
+10.times do |num|
+  next if num == 5
+  puts num
+end
+
+#redo
+3.times do |num|
+  puts "num of tries= #{num}"
+  puts "Do you want to redo (y/n):"
+  option = gets.chop
+  redo if option == 'y'
+end
